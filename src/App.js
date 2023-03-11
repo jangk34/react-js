ca6dcf2673ddd894c05bb2478ff7bc6a02659a24
jangk34 <jangk34@naver.con>
2023-03-12 00:12:35 +0900

    o

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/hello" element={<h1>"HELLO !!!"</h1>} />
-        <Route path={`${process.env.PUBLIC_URL}/`} element={Home} />
+        <Route basename={`${process.env.PUBLIC_URL}/`} element={Home} />
       </Routes>
     </Router>
   );
